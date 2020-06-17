cce6ff9e9b70c0136c0a91c4f08090409a786812
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-06-17 15:53:49 +0700

    verify user token

---
+++ app/middlewares/verify-token.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const verifyToken = async (req, res, next) => {
+    try {
+        //get token from request header
+        const bearer = req.header('Authorization');
+        if (!bearer) {
+            res.status(200).json({"code": 1, "message": "Token is required", "data": null})
+        }
+        // delete text Bearer inside token
+        const token = bearer.replace('Bearer ', '');
+        
+        // verify token
+        const decoded = await jwt.verify(token, process.env.JWT_KEY);
+        // bind decode in req   
+        req.decoded = decoded;
+        next();
+        
+    }catch (err) {
+        // message if token is expired
+        res.status(200).json({"code": 1, "message": `${err.message}`, "data": null })
+        
+    }
+}
+
+module.exports = verifyToken
