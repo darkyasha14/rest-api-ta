7e0c4dac14a678bee0f2c07d2f4e62617fdf8863
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-06-17 15:55:20 +0700

    update post for login and add aiAuth

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,14 +1,25 @@
 const {Router} = require("express")
 
 const router = Router()
+
 const controllerUser = require('../controllers/controllers-user')
+const controllersHotel = require('../controllers/controllers-hotel')
 
+const isAuthenticate = require('../middlewares/verify-token')
+const auth = require('../middlewares/authenticate')
 
-router.get('/user/:id', controllerUser.getUserById)
-router.get('/user/', controllerUser.getAllUser)
+router.post('/auth/', auth.login)
+
+router.get('/user/:id', isAuthenticate, controllerUser.getUserById)
+router.get('/user/', isAuthenticate, controllerUser.getAllUser)
 router.post('/user', controllerUser.createNewUser)
 router.put('/user/:id', controllerUser.updateUser)
 router.delete('/user/:id', controllerUser.delelteUser)
+router.get('/hotel/', controllersHotel.getAllRomm)
+router.get('/hotel/:id', controllersHotel.getRoomById)
+router.post('/hotel/', controllersHotel.createNewRoom)
+router.put('/hotel/:id', controllersHotel.updateRoom)
+router.delete('/hotel/:id', controllersHotel.deleteRoom)
 
 
 module.exports = router
