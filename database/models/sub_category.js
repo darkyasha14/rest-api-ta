75adfb92346069e880db1b5a81a705b4748e0a16
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-06-19 22:21:59 +0700

    add models sub_category

---
+++ database/models/sub_category.js
@@ -0,0 +1,59 @@
+'use strick'
+
+module.exports = (sequelize, DataTypes) => {
+    const Sub_category = sequelize.define('Sub_category', {
+        sub_category_id : {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category_id : {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        sub_category_name : {
+            type: DataTypes.STRING(20),
+            allowNull : false,
+            unique: {
+                args : true,
+                msg : "name of sub category already exist"
+
+            },
+        },
+        sub_category_desc : {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        createdAt:{
+            type: DataTypes.DATE,
+            allowNull: true, 
+            defaultValue: DataTypes.DATE.NOW
+        },
+        updatedAt:{
+            type: DataTypes.DATE,
+            allowNull: true, 
+            defaultValue: DataTypes.DATE.NOW
+        }
+    },
+    {
+        tableName : 't_sub_category',
+        underscored: true
+    })
+
+    Sub_category.removeAttribute('id')
+
+    Sub_category.associate = (models) =>{
+        Sub_category.belongsTo(models.Category, {
+            foreignKey: 'category_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        }),
+        Sub_category.hasMany(models.Jasa, {
+            foreignKey: 'sub_category_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        })
+    }
+
+    return Sub_category
+}
