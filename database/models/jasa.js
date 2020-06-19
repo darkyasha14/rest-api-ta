58a272ff7cd0d90a7a9b0d86459c1f7f51897c17
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-06-19 22:23:57 +0700

    add models jasa

---
+++ database/models/jasa.js
@@ -0,0 +1,64 @@
+'use strick'
+
+module.exports = (sequelize, DataTypes) => {
+    const Jasa = sequelize.define('Jasa', {
+        jasa_id : {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        sub_category_id : {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        jasa_name : {
+            type: DataTypes.STRING(20),
+            allowNull : false,
+            unique: {
+                args : true,
+                msg : "name of jasa already exist"
+
+            },
+        },
+        jasa_desc : {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        jasa_price : {
+            type : DataTypes.INTEGER,
+            allowNull: false,
+            validate : {
+                isNumeric: {
+                    args : true,
+                    msg : "only allow numberic for price"
+                }
+            } 
+        },
+        createdAt:{
+            type: DataTypes.DATE,
+            allowNull: true, 
+            defaultValue: DataTypes.DATE.NOW
+        },
+        updatedAt:{
+            type: DataTypes.DATE,
+            allowNull: true, 
+            defaultValue: DataTypes.DATE.NOW
+        }
+    },
+    {
+        tableName: 't_jasa',
+        undersrored: true
+    })
+
+    Jasa.removeAttribute('id')
+
+    Jasa.associate = (models) => {
+        Jasa.belongsTo(models.Sub_category, {
+            foreignKey: 'sub_category_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        })
+    }
+
+    return Jasa
+}
