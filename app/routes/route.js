09640e307c19b9c5dc7f4569290d816875d529b9
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-06-20 13:23:29 +0700

    Update route.js

--- app/routes/route.js
+++ app/routes/route.js
@@ -6,6 +6,7 @@ const controllerUser = require('../controllers/controllers-user')
 const controllersHotel = require('../controllers/controllers-hotel')
 const controllersCategory = require('../controllers/controllers-category')
 const controllersSubCategory = require('../controllers/controllers-sub_category')
+const controllersJasa = require('../controllers/controllers-jasa')
 
 const isAuthenticate = require('../middlewares/verify-token')
 const auth = require('../middlewares/authenticate')
@@ -32,7 +33,12 @@ router.put('/category/:id', controllersCategory.updateCategory)
 router.get('/sub-category/',    controllersSubCategory.getSubCategorylist)
 router.get('/sub-category/:id', controllersSubCategory.getSubCategoryById)
 router.post('/sub-category/',   controllersSubCategory.createSubCategory)
-router.put('/category/:id',     controllersSubCategory.updateSubCategory)
+router.put('/sub-category/:id',     controllersSubCategory.updateSubCategory)
+
+router.get('/jasa/',        controllersJasa.getJasalist)
+router.get('/jasa/:id',     controllersJasa.getJasaById)
+router.post('/jasa/',       controllersJasa.createJasa)
+router.put('/jasa/:id',     controllersJasa.updateJasa)
 
 
 
