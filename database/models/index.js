15c2aff47ba4b389712de9d264c3dbf5d90ed0a4
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-06-25 21:59:36 +0700

    update index

--- database/models/index.js
+++ database/models/index.js
@@ -15,15 +15,26 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname).filter(file => {
+  // console.log(file)
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
+  }).forEach(file => {
     const model = sequelize['import'](path.join(__dirname, file));
+    console.log(file)
     db[model.name] = model;
   });
+// fs.readdirSync(__dirname).filter((file) =>
+//   file !== 'index.js'
+// ).forEach((file) => {
+//   const model = sequelize.import(path.join(__dirname, file))
+//     console.log("model " + model)
+//   try{
+//     db[model.name] = model
+//     console.log("working")
+//   }catch(err){
+//     console.error(err)
+//   }
+// })
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
