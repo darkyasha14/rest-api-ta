cd78dbb09f2e86557235e204af396ed0ee732a20
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-07-21 00:54:22 +0700

    add url file image n delete sub category

--- app/controllers/controllers-sub_category.js
+++ app/controllers/controllers-sub_category.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const fs = require('fs')
+require('dotenv').config()
 
 const models = require('../../database/models')
+const domain = require('./../helper/getDomain')
 
 const getSubCategorylist = async(req, res) => {
     try {
@@ -81,15 +83,18 @@ const createSubCategory = async(req, res) => {
         // jika dalam request terdapat file
         console.log(req.file)
         if(req.file){
-            const tempPath = await req.file.path                                // ambil file path setelah di upload di folder tmp
-            const targetPath = await path.join(__dirname, './../../public/image/' + titleImg(sub_category_name) + ".png")        // ganti setiap file yg di upload menjadi .png
-            console.log(targetPath)
+            const domainName = await domain.getFullDomainURL(req)
+
+            const tempPath = await req.file.path                                                                       // ambil file path setelah di upload di folder tmp
+            const targetPath = await path.resolve(process.env.IMG_PATH_UPLOAD) + '/' + titleImg(sub_category_name) + ".png"        // ganti setiap file yg di upload menjadi .png
+            const urlFile = await domainName + process.env.IMG_PATH_UPLOAD + titleImg(sub_category_name) + '.png'                      // buat url untuk image tsb
+            console.log(urlFile)
 
             const data = await models.Sub_category.create({
                 category_id: category_id,
                 sub_category_name: sub_category_name,
                 sub_category_desc: sub_category_desc,
-                img_url : targetPath,
+                img_url : urlFile,
             })
         
                 if(data){
@@ -97,11 +102,25 @@ const createSubCategory = async(req, res) => {
                     await fs.rename(tempPath, targetPath, err => {
                         if (err){
                             console.log(err);
-                            return res.send(500)
                         }
                     })
 
-                    return res.status(201).json({code: 0, message: 'success add new sub_category', data: data})
+                    const getData = await models.Sub_category.findOne({
+                        where : 
+                        {
+                            sub_category_id : data.dataValues.sub_category_id
+                        },
+                        include : [ 
+                            {
+                                model: models.Jasa
+                            },
+                            {
+                                model: models.Category
+                            }
+                        ]
+                    })
+
+                    return res.status(201).json({code: 0, message: 'success add new sub_category', data: getData})
                 }else{
                     return res.json({code: 1, message: 'failled added new sub_category', data: null})
                 }
@@ -113,19 +132,33 @@ const createSubCategory = async(req, res) => {
             })
 
             if(data){
-                return res.status(201).json({code: 0, message: 'new post successfully added, no file uploaded', data})
+                const getData = await models.Sub_category.findOne({
+                    where : 
+                    {
+                        sub_category_id : data.dataValues.sub_category_id
+                    },
+                    include : [ 
+                        {
+                            model: models.Jasa
+                        },
+                        {
+                            model: models.Category
+                        }
+                    ]
+                })
+                return res.status(201).json({code: 0, message: 'new post successfully added, no file uploaded', getData})
             }else{
                 return res.json({code: 1, message: "new post failed added", data: null})
             }
         }
     } catch (error) {
         console.log(error)
-        if(error.errors){
-            if(req.file){
-                const tempPath = await req.file.path
-                fs.unlink(tempPath, err => console.log(err))
-            }
+        if(req.file){
+            const tempPath = await req.file.path
+            fs.unlink(tempPath, err => console.log(err))
+        }
 
+        if(error.errors){
             return res.json({code: 1, message: error.errors[0].message, data: null})
         }else {
             return res.json({code: 1, message: error, data: null})
@@ -141,46 +174,58 @@ const updateSubCategory = async(req, res) => {
         const data = await models.Sub_category.findOne({where : {sub_category_id : id}})
         if(data){
             if(req.file){
-                const tempPath = await req.file.path
-                const targetPath = await path.join(__dirname, './../../public/image/' + titleImg(sub_category_name) + ".png")
+                const domainName = await domain.getFullDomainURL(req)
+
+                const tempPath = await req.file.path                                                                       // ambil file path setelah di upload di folder tmp
+                const targetPath = await path.resolve(process.env.IMG_PATH_UPLOAD) + '/' + titleImg(sub_category_name) + ".png"        // ganti setiap file yg di upload menjadi .png
+                const urlFile = await domainName + process.env.IMG_PATH_UPLOAD + titleImg(sub_category_name) + '.png'                      // buat url untuk image tsb
+                console.log(urlFile)                       // buat url untuk image tsb
   
                 const update = await models.Sub_category.update({
                     sub_category_name: sub_category_name,
                     sub_category_desc: sub_category_desc,
-                    img_url: targetPath,
+                    img_url: urlFile,
                     update_at: new Date()
                 },{where : {sub_category_id: id}})
     
                 // jika data success di update
                 if(update){
-                    
-                    // delete image yg lama, dengan mengambil path yg lama pada column thumbnail_url
-                    await fs.unlink(data.dataValues.img_url, err => {
-                        if(err){
-                            console.log(err)
-                        }
-                    })
-
-                    // update dg image yg baru
-                    await fs.rename(tempPath, targetPath, err => {
-                        if (err){
-                            console.log(err);
-                            return res.send(500)
-                        }
-                    })
-
-                    const updateData = await models.Sub_category.findOne({where: {sub_category_id: id},
+                    const uploadData = await models.Sub_category.findOne({
+                        where : 
+                        {
+                            sub_category_id : data.dataValues.sub_category_id
+                        },
                         include : [ 
                             {
-                                model: models.Jasa,
+                                model: models.Jasa
                             },
                             {
-                                model: models.Category, attributes : ['category_name', 'category_desc']
+                                model: models.Category
                             }
                         ]
                     })
 
-                    return res.status(201).json({code: 0, message: 'post successfully updated', data: updateData})
+                    if(data.dataValues.img_url !== null){
+                        const imgName = path.basename(data.dataValues.img_url, '.png')                            // get filename yg berformat .png
+                        const imgPath = path.resolve(process.env.IMG_PATH_UPLOAD) + '/' + imgName + '.png' 
+                    
+                        // delete image yg lama, dengan mengambil path yg lama pada column thumbnail_url
+                        await fs.unlink(imgPath, err => {
+                            if(err){
+                                console.log(err)
+                        }
+                    })
+                    }
+                    
+                    
+                    // update dg image yg baru
+                    await fs.rename(tempPath, targetPath, err => {
+                        if (err){
+                            console.log(err);
+                        }
+                    })
+
+                    return res.status(201).json({code: 0, message: 'post successfully updated', data: uploadData})
                 }else{
                     return res.json({code: 1, message: "post failed updated", data: null})
                 }
@@ -192,9 +237,22 @@ const updateSubCategory = async(req, res) => {
                 },{where : {sub_category_id: id}})
     
                 if(update){
-                    const updateData = await models.Sub_category.findOne({where : {sub_category_id: id}})
+                    const uploadData = await models.Sub_category.findOne({
+                        where : 
+                        {
+                            sub_category_id : data.dataValues.sub_category_id
+                        },
+                        include : [ 
+                            {
+                                model: models.Jasa
+                            },
+                            {
+                                model: models.Category
+                            }
+                        ]
+                    })
 
-                    return res.status(201).json({code: 0, message: 'successfully updated, no files updated', data: updateData})
+                    return res.status(201).json({code: 0, message: 'successfully updated, no files updated', data: uploadData})
                 }else{
                     return res.json({code: 1, message: "post failed updated", data: null})
                 }
@@ -204,21 +262,61 @@ const updateSubCategory = async(req, res) => {
         }
     } catch (error) {
         console.log(error)
+        if(req.file){
+            const tempPath = await req.file.path
+            fs.unlink(tempPath, err => console.log(err))
+        }
         if(error.errors){
-            if(req.file){
-                const tempPath = await req.file.path
-                fs.unlink(tempPath, err => console.log(err))
-            }
-
+           
             return res.json({code: 1, message: error.errors[0].message, data: null})
         }
         else return res.json({code: 1, message: error, data: null})
     }
 }
 
+const deleteSubCategory = async (req, res) => {
+    try {
+        
+        const { id } = req.params
+
+        const data = await models.Sub_category.findOne({ where: {sub_category_id: id} })
+
+        if(data){
+            const deleteData = await models.Sub_category.destroy({ where: { sub_category_id: id} })
+    
+            if(deleteData){
+                if(data.dataValues.img_url !== null){
+                    const imgName = path.basename(data.dataValues.img_url, '.png')                            // get filename yg berformat .png
+                    const imgPath = path.resolve(process.env.IMG_PATH_UPLOAD) + '/' + imgName + '.png'              // get full file path
+
+                    fs.unlink(imgPath, err => {
+                        if(err){
+                            console.log(err)
+                        }
+                    })
+                }
+
+                return res.json({code: 0, message: 'data successfully deleted', data: data})
+            }else{
+                return res.json({code: 1, message: 'data failed deleted', data: null})
+            }
+        }else{
+            return res.json({code: 1, message: "data with the specified ID does not exists", data: null})
+        }
+
+    } catch (error) {
+        if(error.message) return res.send({code: 1, message: error.message, data: null})
+        else return res.send({code: 1, message: error, data: null})
+    }
+}
+
+
+
+
 module.exports = {
     getSubCategorylist,
     getSubCategoryById,
     createSubCategory,
-    updateSubCategory
+    updateSubCategory,
+    deleteSubCategory
 }
