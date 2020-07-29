f608c242a9f4cf3d1f06056a4cdd92f77a243fe7
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-07-29 12:54:15 +0700

    add upload image profile

--- app/controllers/controllers-user_profil.js
+++ app/controllers/controllers-user_profil.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const fs = require('fs')
+require('dotenv').config()
 
 const models = require('../../database/models')
+const domain = require('./../helper/getDomain')
 
 const getProfilDetail = async(req, res) => {
     try {
@@ -24,16 +26,6 @@ const getProfilDetail = async(req, res) => {
             return res.status(400).json({code: 1, message: error, data: null})
         }
     }
-}
-
-const titleImg = (paramater) => {
-    var resTitle = ''
-    paramater.substring(0, 19).split(' ').forEach(element => {
-        resTitle += `${element}-`
-    });
-
-    console.log("title image : ", resTitle.substring(0, resTitle.length - 1))
-    return resTitle.substring(0, resTitle.length - 1)
 }
 
 const getProfilbuUserID = async ( req,res) => {
@@ -62,6 +54,15 @@ const getProfilbuUserID = async ( req,res) => {
         }
     }
 }
+const titleImg = (paramater) => {
+    var resTitle = ''
+    paramater.substring(0, 19).split(' ').forEach(element => {
+        resTitle += `${element}-`
+    });
+
+    console.log("title image : ", resTitle.substring(0, resTitle.length - 1))
+    return resTitle.substring(0, resTitle.length - 1)
+}
 
 const createProfil = async(req, res) => {
     try {
@@ -70,14 +71,17 @@ const createProfil = async(req, res) => {
         // jika dalam request terdapat file
         console.log(req.file)
         if(req.file){
-            const tempPath = await req.file.path                                // ambil file path setelah di upload di folder tmp
-            const targetPath = await path.join(__dirname, './../../public/image/' + titleImg(phone) + ".png")        // ganti setiap file yg di upload menjadi .png
-            console.log(targetPath)
+            const domainName = await domain.getFullDomainURL(req)
 
-            const data = await models.Sub_category.create({
+            const tempPath = await req.file.path                                                                       // ambil file path setelah di upload di folder tmp
+            const targetPath = await path.resolve(process.env.IMG_PATH_UPLOAD) + '/' + titleImg(user_id) + ".png"        // ganti setiap file yg di upload menjadi .png
+            const urlFile = await domainName + "/" + process.env.IMG_PATH_UPLOAD + titleImg(user_id) + '.png'                      // buat url untuk image tsb
+            console.log(urlFile)
+
+            const data = await models.Profil.create({
                 user_id: user_id,
                 phone: phone,
-                user_img : targetPath,
+                user_img : urlFile,
             })
         
                 if(data){
@@ -85,13 +89,24 @@ const createProfil = async(req, res) => {
                     await fs.rename(tempPath, targetPath, err => {
                         if (err){
                             console.log(err);
-                            return res.send(500)
                         }
                     })
+                    
+                    const getData = await models.Profil.findOne({
+                        where : 
+                        {
+                            profil_id : data.dataValues.profil_id
+                        },
+                        include : [ 
+                            {
+                                model: models.User
+                            }
+                        ]
+                    })
 
-                    return res.status(201).json({code: 0, message: 'success add new sub_category', data: data})
+                    return res.status(201).json({code: 0, message: 'success add profil', data: getData})
                 }else{
-                    return res.json({code: 1, message: 'failled added new sub_category', data: null})
+                    return res.json({code: 1, message: 'failled to add profil', data: null})
                 }
         }else{
             const data = await models.Profil.create({
@@ -100,19 +115,30 @@ const createProfil = async(req, res) => {
             })
 
             if(data){
-                return res.status(201).json({code: 0, message: 'new post successfully added, no file uploaded', data})
+                const Data = await models.Profil.findOne({
+                    where : 
+                    {
+                        profil_id : data.dataValues.profil_id
+                    },
+                    include : [ 
+                        {
+                            model: models.User
+                        }
+                    ]
+                })
+                return res.status(201).json({code: 0, message: 'profil added, no file uploaded', Data})
             }else{
-                return res.json({code: 1, message: "new post failed added", data: null})
+                return res.json({code: 1, message: "new profil failed added", data: null})
             }
         }
     } catch (error) {
         console.log(error)
-        if(error.errors){
-            if(req.file){
-                const tempPath = await req.file.path
-                fs.unlink(tempPath, err => console.log(err))
-            }
+        if(req.file){
+            const tempPath = await req.file.path
+            fs.unlink(tempPath, err => console.log(err))
+        }
 
+        if(error.errors){
             return res.json({code: 1, message: error.errors[0].message, data: null})
         }else {
             return res.json({code: 1, message: error, data: null})
