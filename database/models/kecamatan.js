6ee77361b523a82da75183a7ed8168c525026e49
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-08-30 18:17:12 +0700

    Create kecamatan.js

---
+++ database/models/kecamatan.js
@@ -0,0 +1,45 @@
+'use strick'
+
+module.exports = (sequelize, DataTypes) => {
+    const Kecamatan = sequelize.define('Kecamatan', {
+        kecamatan_id : {
+            type: DataTypes.STRING(24),
+            primaryKey: true,
+        },
+        kota_id : {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        },
+        nama : {
+            type: DataTypes.STRING(50),
+            allowNull : false,
+           
+        }
+    },
+    {
+        tableName: 't_Kecamatan',
+        undersrored: true
+    })
+
+    Kecamatan.removeAttribute('id')
+
+    Kecamatan.associate = (models) => {
+        Kecamatan.hasMany(models.Address, {
+            foreignKey: 'kecamatan_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        })
+        Kecamatan.belongsTo(models.Kota, {
+            foreignKey: 'kota_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        })
+        Kecamatan.hasMany(models.Kelurahan, {
+            foreignKey: 'kecamatan_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        })
+    }
+
+    return Kecamatan
+}
