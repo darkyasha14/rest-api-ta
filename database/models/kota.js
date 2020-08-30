27734b26f52d28091e702d32a4f5675324613bf7
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-08-30 18:16:53 +0700

    Create kota.js

---
+++ database/models/kota.js
@@ -0,0 +1,36 @@
+'use strick'
+
+module.exports = (sequelize, DataTypes) => {
+    const Kota = sequelize.define('Kota', {
+        kota_id : {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+        },
+        nama : {
+            type: DataTypes.STRING(20),
+            allowNull : false,
+           
+        }
+    },
+    {
+        tableName: 't_kota',
+        undersrored: true
+    })
+
+    Kota.removeAttribute('id')
+
+    Kota.associate = (models) => {
+        Kota.hasMany(models.Address, {
+            foreignKey: 'kota_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        })
+        Kota.hasMany(models.Kecamatan, {
+            foreignKey: 'kota_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        })
+    }
+
+    return Kota
+}
