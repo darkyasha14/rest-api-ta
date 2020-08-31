3ce91b6bc6d2ce042728a9d48177f02a588f2de7
irfan kurniwan <60690107+darkyasha14@users.noreply.github.com>
2020-08-31 13:56:09 +0700

    Update controllers-kota.js

--- app/controllers/controllers-kota.js
+++ app/controllers/controllers-kota.js
@@ -2,20 +2,19 @@ const models = require('../../database/models')
 
 
 const createNewKota = async (req, res) => {
-    console.log(req.body);
     
     try {
-        const {kota_id, nama} = req.body
-        
-        const data = await models.Kota.create({
-            kota_id : kota_id,
-            nama : nama,
-        })
-        if(data){
-            return res.json({"code" : 0, "message" : "success add new kota", "data" : data})
-        }else{
-            return res.json({"code" : 1, "message" : "failded add new address", "data" : null})
-        }
+        const { list_kota } = req.body
+
+        list_kota.forEach(element => {
+            console.log(element)
+            models.Kota.create({
+                kota_id : element.id,
+                nama : element.nama,
+            })
+        });
+
+        return res.json({"code" : 0, "message" : "success add new kota", "data" : list_kota})
     } catch (error) {
         console.log(error);
         if(error.message){
@@ -26,6 +25,66 @@ const createNewKota = async (req, res) => {
     }
 }
 
+const getKotaDetail = async (req, res) => {
+    console.log(req.params);
+    
+    try {
+        const {id} = req.params
+        const data = await models.Kota.findOne({where : {kota_id: id}})
+        if(data){
+            return res.json({"code" : 0, "message" : "Successfully get kota detail", "data" : data})
+        }else{
+            return res.json({"code" : 1, "message" : "data with specified id not found", "data" : null})
+        }
+
+    } catch (error) {
+        if(error.message){
+            return res.json({"code" : 1, "message" : error.message, "data" : null})
+        }else{
+            return res.json({"code" : 1, "message": error, "data" : null })
+        }
+    }
+}
+
+const getAllKota = async (req, res) => {
+    try {
+        const data = await models.Kota.findAll()
+
+        if(data.length > 0)
+            return res.json({code: 0, message: "success get all kota", data: data})
+        else
+            return res.json({code: 1, message: 'no data found, table is empty', data: null})
+    } catch (error) {
+        console.log(error);
+        if(error.message){
+            return res.json({"code" : 1, "message" : error.message, "data" : null})
+        }else{
+            return res.json({"code" : 1, "message" : error, "data" : null})
+        } 
+    }
+}
+
+
+// Menghapus seluruh isi table kota
+const truncateTableKota = async (req, res) => {
+    try {
+        await models.Kota.destroy({truncate: { cascade : true }, restartIdentity: true})
+        
+        return res.json({code: 0, message: 'all data kota successfully truncate', data: null})
+    } catch (error) {
+        console.log(error);
+        if(error.message){
+            return res.json({"code" : 1, "message" : error.message, "data" : null})
+        }else{
+            return res.json({"code" : 1, "message" : error, "data" : null})
+        } 
+    }
+}
+
+
 module.exports = {
-    createNewKota
+    createNewKota,
+    getAllKota,
+    truncateTableKota,
+    getKotaDetail
 }
